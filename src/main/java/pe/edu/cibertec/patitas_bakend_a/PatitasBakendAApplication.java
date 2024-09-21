90d285163da089a5744509ef0d176bd26118854b
Daniel Isidro <isidroesteban402@gmail.com>
2024-09-20 20:11:21 -0500

    Patitas backend-a

---
+++ src/main/java/pe/edu/cibertec/patitas_bakend_a/PatitasBakendAApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas_bakend_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasBakendAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitasBakendAApplication.class, args);
+	}
+
+}
